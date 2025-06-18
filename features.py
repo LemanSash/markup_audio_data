5e1c4d7fa6aa0843c2b6cd2abda54cbaa6524bf7
LemanSash <leman.sash@ya.ru>
2025-06-18 22:41:48 +0300

    changed features

--- features.py
+++ features.py
@@ -1,26 +1,10 @@
 import numpy as np
 import librosa
-import python_speech_features
+from python_speech_features import mfcc, delta
 
-def extract_frame_features(y, sr):
-    # MFCC
-    mfcc = python_speech_features.mfcc(y, samplerate=sr, winstep=512 / sr)
-    
-    # Energy
-    energy = np.array([
-        np.sum(np.square(y[i * 512: (i + 1) * 512]))
-        for i in range(mfcc.shape[0])
-    ]).reshape(-1, 1)
-    
-    # Pitch (using librosa's piptrack)
-    pitches, magnitudes = librosa.piptrack(y=y.astype(float), sr=sr, hop_length=512)
-    pitch = []
-    for i in range(pitches.shape[1]):
-        index = magnitudes[:, i].argmax()
-        pitch_val = pitches[index, i]
-        pitch.append(pitch_val)
-    pitch = np.array(pitch).reshape(-1, 1)
-    
-    # Объединение всех признаков
-    features = np.hstack([mfcc, energy, pitch])
-    return features
+def extract_frame_features(signal, sample_rate):
+    mfcc_feat = mfcc(signal, samplerate=sample_rate, numcep=13)
+    d_mfcc_feat = delta(mfcc_feat, 2)
+    dd_mfcc_feat = delta(d_mfcc_feat, 2)
+    combined = np.hstack((mfcc_feat, d_mfcc_feat, dd_mfcc_feat))  # shape: (frames, 39)
+    return combined
